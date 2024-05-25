98f58505891b1cbb63b1877ae14b1e12a9fc4ad8
Bhadresh <Bhadresh@host.docker.internal>
2024-05-25 14:02:07 -0700

    Second Commit

---
+++ music-app/src/main/java/com/muic_app/Configuration/WebSocketConfiguration.java
@@ -0,0 +1,25 @@
+package com.muic_app.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration  implements WebSocketMessageBrokerConfigurer{
+	
+
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+		.setAllowedOriginPatterns("*")
+		.withSockJS();
+	}
+	
+	public void configureMessageBroke(MessageBrokerRegistry config ) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+
+}
